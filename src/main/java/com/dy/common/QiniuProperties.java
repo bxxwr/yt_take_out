a28f05d8197b63c288cced58c1c9351cc2da55d6
DavidYoung <321740709@qq,com>
2022-06-19 16:04:47 +0800

    小程序后端，更新了一些登录操作

---
+++ src/main/java/com/dy/common/QiniuProperties.java
@@ -0,0 +1,19 @@
+package com.dy.common;
+
+import lombok.Data;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "qiniu")
+@Data
+public class QiniuProperties {
+    private String accessKey;
+
+    private String secretKey;
+
+    private String bucket;
+
+
+
+}
